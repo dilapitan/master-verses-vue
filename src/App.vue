466051b88c48ec62907d1ae978b49064e4424a59
dilapitan <domlapitan@gmail.com>
2020-08-02 17:58:17 +0800

    Change header style

--- src/App.vue
+++ src/App.vue
@@ -48,9 +48,11 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar app>
+    <v-app-bar app color="primary">
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Master Verses</v-toolbar-title>
+      <v-toolbar-title>
+        <strong>Master Verses</strong>
+      </v-toolbar-title>
     </v-app-bar>
 
     <v-main>
