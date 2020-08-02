eaab5063208953c1a7fbd0570967f8b5723f6039
dilapitan <domlapitan@gmail.com>
2020-08-02 17:34:15 +0800

    Display the subcategory per category

--- src/components/SubCategoryList.vue
+++ src/components/SubCategoryList.vue
@@ -1,16 +1,27 @@
 <template>
-  <div>A verse</div>
+  <div>
+    <div :class="`text-xs-caption text-sm-subtitle-1`">
+      {{ trimmedSubCategory(verses.subCategory) }}
+    </div>
+  </div>
 </template>
 
 <script>
+import MemoryVerse from '@/components/MemoryVerse'
+
 export default {
   name: 'SubCategoryList',
 
+  component: {
+    MemoryVerse,
+  },
+
   props: ['verses'],
 
-  created() {
-    console.log(this.verses)
-    //console.log('----------')
+  methods: {
+    trimmedSubCategory(subcategory) {
+      return subcategory.replace(/_/g, ' ').replace(/\w\./i, '')
+    },
   },
 }
 </script>
