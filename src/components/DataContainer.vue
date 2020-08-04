03817490eef1594bc91fd36f83bc190ea92f18a4
dilapitan <domlapitan@gmail.com>
2020-08-04 19:32:10 +0800

    Add a parent container for the memory verses and randomize view

---
+++ src/components/DataContainer.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <v-row>
+      <v-col cols="9" xs="9" sm="3">
+        <v-select
+          v-model="category"
+          label="Select Category"
+          :items="categories"
+        ></v-select>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <template v-for="cv in categorizedVerses">
+          <CategoryList :key="cv.category" :category="cv.category" :cv="cv" />
+        </template>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import CategoryList from '@/components/CategoryList'
+
+export default {
+  name: 'DataContainer',
+
+  components: {
+    CategoryList
+  },
+
+  props: ['verses'],
+
+  data: () => ({
+    category: 'All',
+    categories: [
+      'All',
+      'A. Christ – the Source',
+      'B. Abundant Resources',
+      'C. Maturity Needs',
+      'D. Godly Character',
+      'E. Ministry Basics',
+      'F. Kingdom Commitments',
+      'G. Circles of Influence'
+    ]
+  }),
+
+  computed: {
+    categorizedVerses: function() {
+      if (this.category === 'All') return this.verses
+      const sorted = this.verses.filter((categorizedVerses) => {
+        return categorizedVerses.category === this.category
+      })
+
+      return sorted
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,61 +6,20 @@
 
     <br />
 
-    <v-row>
-      <v-col cols="9" xs="9" sm="3">
-        <v-select
-          v-model="category"
-          label="Select Category"
-          :items="categories"
-        ></v-select>
-      </v-col>
-    </v-row>
-
-    <v-row>
-      <v-col>
-        <template v-for="cv in categorizedVerses">
-          <CategoryList :key="cv.category" :category="cv.category" :cv="cv" />
-        </template>
-      </v-col>
-    </v-row>
+    <DataContainer :verses="verses" />
   </v-container>
 </template>
 
 <script>
-import CategoryList from '@/components/CategoryList'
+import DataContainer from '@/components/DataContainer'
 
 export default {
   name: 'Home',
 
   components: {
-    CategoryList,
+    DataContainer
   },
 
-  props: ['verses'],
-
-  data: () => ({
-    category: 'All',
-    categories: [
-      'All',
-      'A. Christ – the Source',
-      'B. Abundant Resources',
-      'C. Maturity Needs',
-      'D. Godly Character',
-      'E. Ministry Basics',
-      'F. Kingdom Commitments',
-      'G. Circles of Influence',
-    ],
-  }),
-
-  computed: {
-    categorizedVerses: function() {
-      if (this.category === 'All') return this.verses
-      const sorted = this.verses.filter((categorizedVerses) => {
-        return categorizedVerses.category === this.category
-      })
-
-      return sorted
-    },
-  },
+  props: ['verses']
 }
 </script>
